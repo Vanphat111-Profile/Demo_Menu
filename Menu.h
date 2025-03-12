440e58122396cd5e87dafe5f31bece3c21b08da1
Vanphat111 <phathovan14122006+github@gmail.com>
2025-03-12 16:35:46 +0700

    Add source code Demo_Menu.

---
+++ Menu.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Menu
+{
+private:
+	string tieude;
+	vector<string> dsLuachon; //danh sach cac lua chon
+public:
+	Menu(string tieude);
+	void themLuachon(string luachon);//them mot chuc nang cho thuc don
+	Menu& operator<<(const string str);
+	void xuat();//xuat danh sach cac lua chon
+	int chon();// xuat danh sach cac muc va cho nguoi dung nhap lua chon
+};
+
